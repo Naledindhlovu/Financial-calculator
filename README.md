eb5af38db85256621a346d96096b2832afc2cb25
Xolani Sithole <89644042+Mondiserh@users.noreply.github.com>
2024-07-10 16:11:35 +0200

    update

---
+++ README.md
@@ -0,0 +1,46 @@
+## **Financial Calculator**
+
+
+## Description
+Creating a program that provides users with access to two different financial calculators an investment calculator and a home loan repayment calculator would offer a comprehensive tool for managing personal finances.
+
+ - The investment calculator would enable users to analyze and project their investment returns, considering factors like initial investment, interest rates, and investment duration.
+ - The home loan repayment calculator would help users determine their monthly mortgage payments, taking into account loan amount, interest rate, and loan term.
+ - This dual-function program would be invaluable for users looking to make informed financial decisions, whether planning for future investments or managing home loan repayments.
+
+## Prerequisites
+
+ - [Python](https://python.org/downloads/)
+ - [Visual Studio Code (VSCode)](https://code.visualstudio.com/Download/)
+
+## Getting Started
+    apt install python
+
+1. Clone the repository:
+
+        git clone https://github.com/Mondiserh/Finance-Calculators.git
+        
+
+2. Open repository in VSCode
+
+3. Run :
+
+    python finance_calculator
+
+
+### How to Calculate
+Follow the calculators prompts
+
+### Screenshot
+
+ ![](image.png)
+ 
+## Author
+
+Xolani Sithole
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+
---
+++ finance_calculators.py
@@ -0,0 +1,82 @@
+#The user  should be allowed to choose which calculation they want to do. 
+# The first output that the user sees when the program runs should look like this :
+import math
+print( "************************************************************************************************")
+print( "***********************************Finicial Calculator******************************************")
+print( "************************************************************************************************")
+print("--Bond to calculate the amount you'll have to pay on a home loan")
+print("--Investment ti calculate the amount of interest you'll earn on interest.")
+print( "************************************************************************************************")
+    
+def get_investment(choose):
+    deposit = int(input('Enter the amount you will like to deposit: '))
+    interest_rate = float(input('Enter the interest rate: '))
+    period= int(input('Enter the period years of investing:  '))
+    print('Choose Simple interest or compound interest')
+    print('1- Simple interest')
+    print('2- Compound interest')
+    choice = int(input('Choice :'))
+    r=interest_rate / 100
+    P=deposit
+    t=period
+
+    while True: 
+      if choice == 1:
+        print( "****************SIMPLE INTEREST****************")
+        A = P * (1 + r * t ) 
+        total= round(A,2)
+        print( f"Total Amount : R{total}")
+        print( "********************************")
+        calc()
+      elif choice == 2:
+        print( "****************COMPOUND INTEREST**************")
+        A = P* math.pow((1+r),t)
+        total= round(A,2)
+        print( f"Total Amount : R{total}")
+        print( "********************************")
+        calc()
+      else: 
+        print('Invalid choice')
+        print('1- Simple interest')
+        print('2- Compound interest')
+        choice = int(input('Choice :'))
+def get_bond(choose):
+    house_value= int(input("Enter Present value of the house: "))
+    interest_rate = float(input('Enter the interest rate: '))
+    period= int(input('Enter the period months to repay the bond:  '))
+    n= period
+    i=interest_rate/12001
+              
+    P = house_value
+    x = (i*P)/1 - math.pow ((1+i),(-n))
+    total = round(x,2)
+    print( f"Monthly repayments : R{total}")
+    print( "********************************")
+    calc()
+
+def calc():
+    print("Choose between bond and investment ")
+    print("1- Investment")
+    print("2- Bond")
+    print("3- EXIT")
+    choose = int(input('Choice :'))
+    while True:
+        
+        if choose == 1:
+          print( "********************INVESTMENT******************")
+          return get_investment(choose)
+        
+        elif choose == 2:
+          print( "**********************BOND*********************")
+          return get_bond(choose)
+          
+        elif choose == 3:
+            print( "***************PROGRAM CLOSED******************")
+            exit()
+        else:
+          print("Choose between bond and investment ")
+          print("1- Investment")
+          print("2- Bond")
+          choose = int(input('Choice :'))
+
+print(calc())
